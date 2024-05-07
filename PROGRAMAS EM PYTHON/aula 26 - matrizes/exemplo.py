0e0084fd36caeab533914ac0f3ce9dc569e1f847
cafeef <cafeef16@gmail.com>
2024-05-07 08:50:27 -0300

    add exemplo aula 26

---
+++ PROGRAMAS EM PYTHON/aula 26 - matrizes/exemplo.py
@@ -0,0 +1,8 @@
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+linha = 0
+while (linha < 3):
+    coluna = 0
+    while (coluna < 3):
+        print(matriz[linha][coluna])
+        coluna += 1
+    linha += 1
