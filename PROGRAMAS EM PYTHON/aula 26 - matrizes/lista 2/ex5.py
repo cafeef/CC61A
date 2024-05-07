fb3c8d77036754fb5d0a562a9b02329dcc49a253
cafeef <cafeef16@gmail.com>
2024-05-07 16:01:31 -0300

    add ex 5

---
+++ PROGRAMAS EM PYTHON/aula 26 - matrizes/lista 2/ex5.py
@@ -0,0 +1,9 @@
+'''
+ Crie um programa que leia duas matrizes 4 x 4
+e escreva, em uma terceira matriz, os maiores
+valores de cada posição das matrizes lidas.
+Exemplo
+matrizA[0][0] = 10
+matrizB[0][0] = 15
+matrizC[0][0] = 15
+'''
