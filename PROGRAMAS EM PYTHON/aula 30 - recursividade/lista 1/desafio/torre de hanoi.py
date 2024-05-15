2e619c72b3dd71f2be5e61ba19ba67fc394cc77e
cafeef <cafeef16@gmail.com>
2024-05-15 12:07:55 -0300

    change desafio

--- PROGRAMAS EM PYTHON/aula 30 - recursividade/lista 1/desafio/torre de hanoi.py
+++ PROGRAMAS EM PYTHON/aula 30 - recursividade/lista 1/desafio/torre de hanoi.py
@@ -1,20 +1,7 @@
 '''
 mover 3 discos em três pinos
 '''
-def torre6(mov_disc, d1, d2, d3, pino1, pino2, pino3):
-    if mov_disc == 1:
-            print('Movendo o disco 1 para o pino 3')
-            pino1[0][0] = 0
-            print(pino1[0][0])
-            print(pino1[1][0])
-            print(pino1[2][0])
-            print('#######')
-            pino3[2][0] = 1
-            print(pino3[0][0])
-            print(pino3[1][0])
-            print(pino3[2][0])
-            print('#######')
-
+def torre6(mov_disc, pino1, pino2, pino3):
     if mov_disc == 2:
         print('Movendo o disco 2 para o pino 2')
         pino1[1][0] = 0
@@ -80,8 +67,21 @@ def torre6(mov_disc, d1, d2, d3, pino1, pino2, pino3):
         print(pino3[2][0])
         print('#######')
 
-def torre(mov_disc, d1, d2, d3, pino1, pino2, pino3):
-    if mov_disc == 7:
+def torre(mov_disc, pino1, pino2, pino3):
+    if mov_disc == 1:
+            print('Movendo o disco 1 para o pino 3')
+            pino1[0][0] = 0
+            print(pino1[0][0])
+            print(pino1[1][0])
+            print(pino1[2][0])
+            print('#######')
+            pino3[2][0] = 1
+            print(pino3[0][0])
+            print(pino3[1][0])
+            print(pino3[2][0])
+            print('#######')
+
+    elif mov_disc == 7:
         print('Movendo o disco 1 no pino 3')
         pino1[2][0] = 0
         print(pino1[0][0])
@@ -92,15 +92,12 @@ def torre(mov_disc, d1, d2, d3, pino1, pino2, pino3):
         return pino3
 
     else:  
-        torre6(mov_disc + 1, d1, d2, d3, pino1, pino2, pino3)
+        torre6(mov_disc + 1, pino1, pino2, pino3)
 
 
 #####################
 mov_disc = 1
-d1 = [1]
-d2 = [2]
-d3 = [3]
-pino1 = [d1, d2, d3]
+pino1 = [[1], [2], [3]]
 pino2 = [[0], [0], [0]]
 pino3 = [[0], [0], [0]]
 print('Pino 1: ')
@@ -116,5 +113,5 @@ print(pino3[0])
 print(pino3[1])
 print(pino3[2])
 print('#######')
-pino3 = torre(mov_disc, d1, d2, d3, pino1, pino2, pino3)
+pino3 = torre(mov_disc, pino1, pino2, pino3)
 print(pino3)
