890720d7f4098d2a38aae607c122a1a6047fdb97
cafeef <cafeef16@gmail.com>
2024-05-21 08:45:26 -0300

    add ex1

--- PROGRAMAS EM PYTHON/aula 22 - vetores/exercícios/exercício 1.py
+++ PROGRAMAS EM PYTHON/aula 22 - vetores/exercícios/exercício 1.py
@@ -1,6 +1,33 @@
-numeros = [0] * 10
-n = 0
-while (n < 10):
-    numeros[n] = int(input(f'Digite o {n + 1}° número: '))
-    n += 1
-print(numeros)
+votacao = []
+relatorio = [0] * 3
+print('Bem vindo a cabine de votação. [0] para sair. ')
+print('[1] CANDITADO 1')
+print('[2] CANDITADO 2')
+print('[3] CANDITADO 3')
+voto = int(input('Digite em quer quer votar: '))
+while (voto != 0):
+        if voto == 1:
+            votacao.append(voto) 
+            relatorio[0] += 1
+        if voto == 2: 
+            votacao.append(voto)
+            relatorio[1] += 1
+        if voto == 3:
+            votacao.append(voto)
+            relatorio[2] += 1 
+        voto = int(input('Digite em quer quer votar [0] para sair.: '))        
+print('Aqui está o resultado da eleição: ')
+print(f'CANDITADO 1: {relatorio[0]} votos.')
+print(f'CANDITADO 2: {relatorio[1]} votos.')
+print(f'CANDITADO 3: {relatorio[2]} votos.')
+maiorV = max(relatorio)
+i = 0
+fim = len(relatorio)
+while (fim >= 0):
+    while(i <= (fim -1)):
+        if relatorio[i] == maiorV:
+            CanGanha = i + 1
+        i += 1
+    fim -= 1
+
+print(f'O canditado ganhador foi: CANDITADO {CanGanha} e fez {maiorV} votos.')
