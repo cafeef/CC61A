f5a1d1c6145e16ced0c146ca1e0cb3f8ec21ac75
cafeef <cafeef16@gmail.com>
2024-06-04 10:29:21 -0300

    add ex4

--- PROGRAMAS EM PYTHON/exercícios de revisão prova 2.py/lista chatgpt/ex4.py
+++ PROGRAMAS EM PYTHON/exercícios de revisão prova 2.py/lista chatgpt/ex4.py
@@ -1,3 +1,16 @@
 '''
-
+Desenvolva uma função recursiva para calcular a potência de um número, com base e expoente dados.
 '''
+#Funções
+def CalculoPotencia(n, p):
+    if p == 1:
+        return n
+    else:
+        return n * CalculoPotencia(n, p - 1) 
+
+
+#Programa principal
+n = int(input('Digite o número que deseja calcular a potência: '))
+p = int(input('Digite a potência desejada: '))
+resultado = CalculoPotencia(n, p)
+print(f'O resultado da potência é: {resultado}')
